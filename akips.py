78f791e27ec8f4f4cd5e7408ecbb55ca56db96c3
Matt Haught <dmhaught@ncsu.edu>
2018-03-19 22:27:12 -0400

    update akips script

--- akips.py
+++ akips.py
@@ -4,18 +4,23 @@ import os
 import json
 import requests
 
-akips_server = os.environ['server']
-password = os.environ['pass']
-groups = {}
-with open(os.path.dirname(os.path.realpath(__file__)) + "/groups.txt") as f:
-    groups = f.readlines()
-groups = [x.strip() for x in groups]
-
-url = 'https://{akips_server}/api-db?password={password};cmds=mget+*+*+ping4+PING.icmpState+value+/up/+any+group+{group}'
-
+akips_server = os.environ['AKIPS_HOST']
+password = os.environ['AKIPS_PASS']
 inventory = {'_meta': {'hostvars': {}}}
 
-for group in groups:
+groupurl = 'https://{akips_server}/api-db?password={password};cmds=list+device+group'
+groupresponse = requests.get(groupurl.format(akips_server=akips_server,
+                                        password=password),
+                        proxies={'http': None, 'https': None},
+                        verify=os.path.dirname(os.path.realpath(__file__)) + "/cacert.cer")
+grouplines = groupresponse.text.split('\n')
+
+for group in grouplines:
+    if group == 'maintenance_mode' or group == 'CS-Servers':
+        continue
+
+    url = 'https://{akips_server}/api-db?password={password};cmds=mget+*+*+ping4+PING.icmpState+value+/up/+any+group+{group}'
+
     response = requests.get(url.format(akips_server=akips_server,
                                        password=password,
                                        group=group),
@@ -23,11 +28,12 @@ for group in groups:
                             verify=os.path.dirname(os.path.realpath(__file__)) + "/cacert.cer")
     lines = response.text.split('\n')
     inventory[group] = {'hosts': []}
+
     for line in lines:
         if line == '':
             continue
-        host, _, _, _, data= line.split(' ')
-        ip = data.split(',')[-1]
+        host = line.split(' ')[0]
+        ip = line.split(',')[-1]
         inventory[group]['hosts'].append(host)
         inventory['_meta']['hostvars'][host] = {'ansible_host': ip}
 
