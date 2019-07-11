d778e12521ab708fcd2710e3e721dd54435db94d
Matt Haught <dmhaught@ncsu.edu>
2019-07-11 15:20:53 -0400

    add variable for regex group matching for ansible_network_os

--- akips.py
+++ akips.py
@@ -14,15 +14,13 @@ inventory = {'_meta': {'hostvars': {}}}
 groupurl = 'https://{akips_server}/api-db?password={password};cmds=list+device+group'
 groupresponse = requests.get(groupurl.format(akips_server=akips_server,
                                         password=password),
-                        proxies={'http': None, 'https': None},
-                        verify=os.path.dirname(os.path.realpath(__file__)) + "/cacert.cer")
+                        proxies={'http': None, 'https': None})
 grouplines = filter(None, groupresponse.text.split('\n'))
 
 groupsuperurl = 'https://{akips_server}/api-db?password={password};cmds=list+device+super+group'
 groupsuperresponse = requests.get(groupsuperurl.format(akips_server=akips_server,
                                         password=password),
-                        proxies={'http': None, 'https': None},
-                        verify=os.path.dirname(os.path.realpath(__file__)) + "/cacert.cer")
+                        proxies={'http': None, 'https': None})
 groupsuperlines = filter(None, groupsuperresponse.text.split('\n'))
 
 groups = grouplines + groupsuperlines
@@ -37,8 +35,7 @@ for group in groups:
     response = requests.get(url.format(akips_server=akips_server,
                                        password=password,
                                        group=group),
-                            proxies={'http': None, 'https': None},
-                            verify=os.path.dirname(os.path.realpath(__file__)) + "/cacert.cer")
+                            proxies={'http': None, 'https': None})
     lines = response.text.split('\n')
     inventory[group] = {'hosts': []}
 
@@ -52,9 +49,11 @@ for group in groups:
             x = inventory['_meta']['hostvars'][host]
         except KeyError:
             inventory['_meta']['hostvars'][host] = {'ansible_host': ip}
-        if re.search('IOS', group, re.IGNORECASE):
+        if re.search(os.environ['AKIPS_IOS_REGEX'] if 'AKIPS_IOS_REGEX' in os.environ else 'IOS', group, re.IGNORECASE):
             inventory['_meta']['hostvars'][host].update({'ansible_network_os': 'ios'})
-        if re.search('NX-OS', group, re.IGNORECASE):
+        if re.search(os.environ['AKIPS_NXOS_REGEX'] if 'AKIPS_NXOS_REGEX' in os.environ else 'NX-OS', group, re.IGNORECASE):
             inventory['_meta']['hostvars'][host].update({'ansible_network_os': 'nxos'})
+        if re.search(os.environ['AKIPS_ASA_REGEX'] if 'AKIPS_ASA_REGEX' in os.environ else 'ASA', group, re.IGNORECASE):
+            inventory['_meta']['hostvars'][host].update({'ansible_network_os': 'asa'})
 
-print json.dumps(inventory, indent=4, sort_keys=True)
+print(json.dumps(inventory, indent=4, sort_keys=True))
